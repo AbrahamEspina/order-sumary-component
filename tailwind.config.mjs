78c4cadae4945e8d1d8573b72c111daa4a5966ba
Abraham Espinosa <abrahamespina123@gmail.com>
2023-12-18 13:10:55 -0300

    final

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				paleBlue: 'hsl(225, 100%, 94%)',
+				brightBlue: 'hsl(245, 75%, 52%)',
+				veryPaleBlue: 'hsl(225, 100%, 98%)',
+				desaturatedBlue: 'hsl(224, 23%, 55%)',
+				darkBlue: 'hsl(223, 47%, 23%)',
+			},
+			backgroundImage: {
+        'background-mobile': "url('.src/assets/pattern-background-mobile.svg')",
+        'background-desktop': "url('./src/assets/pattern-background-desktop.svg')",
+      }
+		},
+	},
+	plugins: [],
+}
